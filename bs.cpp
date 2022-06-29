2ab0b068c2c7ca1dc8c0a98eacfc08940b3b83f1
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-06-29 14:45:46 -0500

    Create bs.cpp

---
+++ bs.cpp
@@ -0,0 +1,18 @@
+class BlackScholes{
+public:
+    double P;
+    double X;
+    double t;
+    double v;
+    double r;
+};
+int main() {
+    BlackScholes bs;
+    bs.P = 62;
+    bs.X = 60;
+    bs.t = 40;
+    bs.v = .32;
+    bs.r = .04;
+    double d1;
+    dl = log(bs.P/bs.X) + (bs.r + .5(bs.v)^2)
+}
